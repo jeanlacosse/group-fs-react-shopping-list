7192bded637d9e53a79cbdc6eb15811661b36a87
Jean-Luc LaCosse <jeanlacosse@gmail.com>
2022-06-09 10:56:49 -0500

    working in post

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,14 +2,30 @@ import React from 'react';
 
 import Header from '../Header/Header.jsx'
 import './App.css';
-
+import ItemForm from '../ItemForm/ItemForm.jsx';
 
 function App() {
+
+    let [shoppingList, setShoppingList] = useState([]);
+
+    const addItem = (itemToAdd) => {
+        axios.post('/list', itemToAdd)
+        .then(response => {
+            // here I rerun the axios.GET
+        })
+        .catch(err => {
+            alert('error adding item')
+            console.log('err is', err)
+        })
+    }
+    
+
     return (
         <div className="App">
             <Header />
             <main>
                 <p>Under Construction...</p>
+            <ItemForm  addItem={addItem}/> 
             
             </main>
         </div>
