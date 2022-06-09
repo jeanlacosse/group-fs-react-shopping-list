03969a4e13c4c345357d51cd686a0cb609d824cb
Jean-Luc LaCosse <jeanlacosse@gmail.com>
2022-06-09 09:46:37 -0500

    sql created

--- database.sql
+++ database.sql
@@ -1,2 +1,15 @@
 -- Don't forget to add your create table SQL 
+CREATE TABLE items (
+    id SERIAL PRIMARY KEY,
+    "name" VARCHAR(80) NOT NULL,
+    quantity DECIMAL(5,2),
+    unit VARCHAR(20)
+);
 -- It is also helpful to include some test data
+INSERT INTO items ("name", quantity, unit)
+VALUES 
+('chocolate milk', 3, 'gal'),
+('dog food', 70, 'lb'),
+('CBD Ointment', 4.3, 'oz'),
+('black tea', 8.4, 'oz'),
+('pokemon cards', 20, 'packs');
